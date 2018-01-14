f1de05647adeed2692b232b67b1d8736a5fbc07d
njavig <njavig@gmail.com>
2018-01-13 23:06:40 -0500

    Insert link of the original question

--- 344.py
+++ 344.py
@@ -1,3 +1,7 @@
+"""
+https://www.reddit.com/r/dailyprogrammer/comments/7j33iv/20171211_challenge_344_easy_baumsweet_sequence/
+"""
+
 user_input = input("Enter a number: ")  # user input in decimal integer
 user_input = int(user_input)
 result_list = []
